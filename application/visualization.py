b3aef7a6a5b26b469adde27c7d65b5b3ed55fee9
Adam035 <adamlipian2723@gmail.com>
2024-12-21 01:19:19 +0100

    fix: correct timestamp formatting

--- application/visualization.py
+++ application/visualization.py
@@ -1,23 +1,31 @@
 import matplotlib.pyplot as plt
 from collections import Counter
-from datetime import datetime
+from datetime import datetime, timedelta
+import numpy as np
 
 
 def generate_plot(data, output_file):
     timestamps = []
     for row in data:
-        timestamp = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S").strftime('%Y-%m-%d %H:%M:%S')
+        try:
+            timestamp = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S.%f")
+        except ValueError:
+            timestamp = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
         timestamps.append(timestamp)
-
-    time_counts = Counter(timestamps)
-    times = list(time_counts.keys())
-    counts = list(time_counts.values())
+    min_time = min(timestamps)
+    max_time = max(timestamps)
+    time_delta = (max_time - min_time) / 20
+    bins = [min_time + i * time_delta for i in range(21)]
+    binned_data = np.histogram(timestamps, bins=bins)[0]
 
     plt.figure(figsize=(10, 6))
-    plt.bar(times, counts, color='skyblue')
+    plt.bar(range(len(binned_data)), binned_data, color='skyblue')
     plt.title("Number of detected threats over time")
     plt.xlabel("Time")
     plt.ylabel("Number of detected threats")
-    plt.xticks(rotation=30)
+
+    xticks = [min_time + i * time_delta for i in range(21)]
+    xticklabels = [timestamp.strftime('%Y-%m-%d %H:%M:%S.%f') for timestamp in xticks]
+    plt.xticks(range(len(xticklabels)), xticklabels, rotation=30)
     plt.tight_layout()
     plt.savefig(output_file)
